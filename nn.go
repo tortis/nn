78b29159c934e70f9f52ed210c0c5b4785f95b3d
David Findley <david.findley@ou.edu>
2015-07-08 13:50:17 -0500

    Implemented cost function and gradient ascent function

--- nn.go
+++ nn.go
@@ -1,6 +1,7 @@
 package nn
 
 import (
+	"log"
 	"math"
 	"math/rand"
 	"time"
@@ -37,17 +38,28 @@ type Node struct {
 	in       []*Connection
 	val      float64
 	activate ActivationFunction
+	isBias   bool
 }
 
 type Layer []*Node
 
-func NewLayer(size int, fn ActivationFunction) Layer {
-	ns := make([]*Node, size)
+func newLayer(size int, withBias bool, fn ActivationFunction) Layer {
+	ns := make([]*Node, size, size+1)
 	// Set the activation functions
-	for i, _ := range ns {
+	for i := 0; i < size; i++ {
 		ns[i] = &Node{
+			out:      make([]*Connection, 0),
+			in:       make([]*Connection, 0),
 			activate: fn,
 		}
+	}
+	if withBias {
+		ns = append(ns, &Node{
+			out:    make([]*Connection, 0),
+			in:     make([]*Connection, 0),
+			val:    1.0,
+			isBias: true,
+		})
 	}
 	return ns
 }
@@ -60,21 +72,23 @@ type ANN struct {
 
 func NewANN(inputSize, outputSize int, activate ActivationFunction) *ANN {
 	return &ANN{
-		input:  NewLayer(inputSize, activate),
-		output: NewLayer(outputSize, activate),
+		input:  newLayer(inputSize, true, activate),
+		output: newLayer(outputSize, false, activate),
 	}
 }
 
-func (this *ANN) addHidden(size int, fn ActivationFunction) {
-	this.hidden = append(this.hidden, NewLayer(size, fn))
+func (this *ANN) AddHidden(size int, withBias bool, fn ActivationFunction) {
+	this.hidden = append(this.hidden, newLayer(size, withBias, fn))
 }
 
-func (this *ANN) wire() {
+func (this *ANN) Wire() {
 	// Connect input to output if there are no hidden layers
 	if len(this.hidden) == 0 {
-		for _, inputNode := range this.input {
+		for i := 0; i < len(this.input); i++ {
+			inputNode := this.input[i]
 			// Connect input directly to output
-			for _, outputNode := range this.output {
+			for j := 0; j < len(this.output); j++ {
+				outputNode := this.output[j]
 				c := NewRandConnection(len(this.input), len(this.output), inputNode, outputNode)
 				inputNode.out = append(inputNode.out, c)
 				outputNode.in = append(outputNode.in, c)
@@ -84,8 +98,13 @@ func (this *ANN) wire() {
 	}
 
 	// Connect input to the first hidden layer
-	for _, inputNode := range this.input {
-		for _, hiddenNode := range this.hidden[0] {
+	for i := 0; i < len(this.input); i++ {
+		inputNode := this.input[i]
+		for j := 0; j < len(this.hidden[0]); j++ {
+			hiddenNode := this.hidden[0][j]
+			if hiddenNode.isBias {
+				continue
+			}
 			c := NewRandConnection(len(this.input), len(this.hidden[0]), inputNode, hiddenNode)
 			inputNode.out = append(inputNode.out, c)
 			hiddenNode.in = append(hiddenNode.in, c)
@@ -95,8 +114,13 @@ func (this *ANN) wire() {
 	// Create connections from hidden to hidden
 	if len(this.hidden) >= 2 {
 		for i := 0; i < len(this.hidden)-1; i++ {
-			for _, hiddenNode := range this.hidden[i] {
-				for _, nextHiddenNode := range this.hidden[i+1] {
+			for j := 0; j < len(this.hidden[i]); j++ {
+				hiddenNode := this.hidden[i][j]
+				for k := 0; k < len(this.hidden[i+1]); k++ {
+					nextHiddenNode := this.hidden[i+1][k]
+					if nextHiddenNode.isBias {
+						continue
+					}
 					c := NewRandConnection(len(this.hidden[i]), len(this.hidden[i+1]), hiddenNode, nextHiddenNode)
 					hiddenNode.out = append(hiddenNode.out, c)
 					nextHiddenNode.in = append(nextHiddenNode.in, c)
@@ -106,8 +130,10 @@ func (this *ANN) wire() {
 	}
 
 	// Create connection from hidden to output
-	for _, hiddenNode := range this.hidden[len(this.hidden)-1] {
-		for _, outputNode := range this.output {
+	for i := 0; i < len(this.hidden[len(this.hidden)-1]); i++ {
+		hiddenNode := this.hidden[len(this.hidden)-1][i]
+		for j := 0; j < len(this.output); j++ {
+			outputNode := this.output[j]
 			c := NewRandConnection(len(this.hidden[len(this.hidden)-1]), len(this.output), hiddenNode, outputNode)
 			hiddenNode.out = append(hiddenNode.out, c)
 			outputNode.in = append(outputNode.in, c)
@@ -115,36 +141,181 @@ func (this *ANN) wire() {
 	}
 }
 
-func (this *ANN) run() {
-	// Run input layer
-	for _, inputNode := range this.input {
-		a := inputNode.activate(inputNode.val)
-		for _, con := range inputNode.out {
-			con.to.val += con.weight * a
+func (this *ANN) Predict(input []float64) []float64 {
+	if len(input) > len(this.input) {
+		log.Fatal("The input vector is too long.")
+	} else if len(input) < len(this.input)-1 {
+		log.Fatal("The input vector is too short.")
+	}
+	// Set the input values
+	for i, inval := range input {
+		if this.input[i].isBias {
+			log.Println("Warning: Assigning input value to the input bias node.")
+		}
+		this.input[i].val = inval
+	}
+
+	// Run the network
+	// For each hidden node, compute the sum of all its inputs,
+	// then run that sum through its activation function.
+	for i, _ := range this.hidden {
+		for j := 0; j < len(this.hidden[i]); j++ {
+			hiddenNode := this.hidden[i][j]
+			if hiddenNode.isBias {
+				continue
+			}
+			// Compute the value of the node
+			hiddenNode.val = 0
+			for k := 0; k < len(hiddenNode.in); k++ {
+				hiddenNode.val += hiddenNode.in[k].from.val * hiddenNode.in[k].weight
+			}
+			// Pass the sumed inputs through the activation function
+			hiddenNode.val = hiddenNode.activate(hiddenNode.val)
 		}
 	}
 
-	// Run hidden layers
-	for i, _ := range this.hidden {
-		for _, hiddenNode := range this.hidden[i] {
-			a := hiddenNode.activate(hiddenNode.val)
-			for _, con := range hiddenNode.out {
-				con.to.val += con.weight * a
+	// Compute the output vector
+	output := make([]float64, len(this.output))
+	for i := 0; i < len(this.output); i++ {
+		outputNode := this.output[i]
+		outputNode.val = 0
+		for j := 0; j < len(outputNode.in); j++ {
+			outputNode.val += outputNode.in[j].from.val * outputNode.in[j].weight
+		}
+		outputNode.val = outputNode.activate(outputNode.val)
+		output[i] = outputNode.val
+	}
+	return output
+}
+
+func (this *ANN) Train(feat, targ [][]float64) float64 {
+	gradientAscent := func(costfn func([]float64) float64, weights []float64, step float64) {
+		epsilon := 1e-4
+		feval := costfn(weights)
+		grad := make([]float64, len(weights))
+		newweights := make([]float64, len(weights))
+		numItr := 0
+
+		for {
+			preEval := feval
+			// Calculate the grad
+			for i := 0; i < len(weights); i++ {
+				val := weights[i]
+				weights[i] = val + epsilon
+				right := costfn(weights)
+				weights[i] = val - epsilon
+				left := costfn(weights)
+				weights[i] = val
+				grad[i] = (right - left) / (2 * epsilon)
 			}
+
+			// New weights
+			for i := 0; i < len(weights); i++ {
+				newweights[i] = weights[i] + step*grad[i]
+			}
+			numItr += 1
+
+			// Eval again
+			feval = costfn(newweights)
+			if feval > preEval {
+				// Update weights
+				for i := 0; i < len(weights); i++ {
+					weights[i] = newweights[i]
+				}
+			} else {
+				log.Printf("Stopping after %d iterations.\n")
+				break
+			}
+		}
+	}
+
+	cost := func(weights []float64) float64 {
+		// Load the weights
+		this.LoadWeights(weights)
+
+		// Comput the cost
+		J := 0.0
+		for m := 0; m < len(feat); m++ {
+			prediction := this.Predict(feat[m])
+			sqerr := 0.0
+			for k := 0; k < len(prediction); k++ {
+				sqerr += math.Pow(targ[m][k]-prediction[k], 2)
+			}
+			J += sqerr
+		}
+		J /= float64(len(feat))
+
+		// Regularisation
+		R := 0.0
+		for i := 0; i < len(weights); i++ {
+			R += math.Pow(weights[i], 2)
+		}
+		R *= 0.01 / float64(this.NumConnections())
+
+		return -(J + R)
+	}
+
+	weights := this.SaveWeights()
+	gradientAscent(cost, weights, 1)
+	terr := cost(weights)
+	return terr
+}
+
+func (this *ANN) LoadWeights(weights []float64) {
+	if len(weights) != this.NumConnections() {
+		log.Printf("LoadWeights: Given weights vector does not match number of weights in the network. Given %d expected %d\n", len(weights), this.NumConnections())
+	}
+	wi := 0
+	for i := 0; i < len(this.hidden); i++ {
+		for j := 0; j < len(this.hidden[i]); j++ {
+			hiddenNode := this.hidden[i][j]
+			for k := 0; k < len(hiddenNode.in); k++ {
+				hiddenNode.in[k].weight = weights[wi]
+				wi += 1
+			}
+		}
+	}
+
+	for i := 0; i < len(this.output); i++ {
+		outNode := this.output[i]
+		for j := 0; j < len(outNode.in); j++ {
+			outNode.in[j].weight = weights[wi]
+			wi += 1
 		}
 	}
 }
 
-func (this *ANN) reset() {
-	for i, _ := range this.hidden {
-		for _, hiddenNode := range this.hidden[i] {
-			hiddenNode.val = 0.0
+func (this *ANN) SaveWeights() []float64 {
+	// Count the expected number of weights for efficency.
+	weights := make([]float64, 0, this.NumConnections())
+	for i := 0; i < len(this.hidden); i++ {
+		for j := 0; j < len(this.hidden[i]); j++ {
+			hiddenNode := this.hidden[i][j]
+			for k := 0; k < len(hiddenNode.in); k++ {
+				weights = append(weights, hiddenNode.in[k].weight)
+			}
 		}
 	}
 
-	for _, outputNode := range this.output {
-		outputNode.val = 0.0
+	for i := 0; i < len(this.output); i++ {
+		outNode := this.output[i]
+		for j := 0; j < len(outNode.in); j++ {
+			weights = append(weights, outNode.in[j].weight)
+		}
 	}
+	return weights
+}
+
+func (this *ANN) NumConnections() int {
+	num := len(this.input) * (len(this.hidden[0]) - 1)
+	for i := 0; i < len(this.hidden); i++ {
+		if i == len(this.hidden)-1 {
+			num += len(this.hidden[i]) * len(this.output)
+		} else {
+			num += len(this.hidden[i]) * (len(this.hidden[i+1]) - 1)
+		}
+	}
+	return num
 }
 
 func StepActivation(in float64) float64 {
@@ -155,6 +326,6 @@ func StepActivation(in float64) float64 {
 	}
 }
 
-func LogisticActivation(in float64) float64 {
-	return 1.0 / (1.0 + math.Exp(-10*in))
+func LogisticActivation(x float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-4*x))
 }
--- nn_test.go
+++ nn_test.go
@@ -7,19 +7,72 @@ import (
 
 func TestNNCreate(t *testing.T) {
 	n := NewANN(10, 1, LogisticActivation)
-	n.addHidden(5, LogisticActivation)
-	n.wire()
+	n.AddHidden(5, true, LogisticActivation)
+	if n.NumConnections() != 61 {
+		t.Fatalf("The created network does not have the expected number of connections. %d != %d", 61, n.NumConnections())
+	}
+	n.Wire()
 }
 
 func TestNNRun(t *testing.T) {
+	n := NewANN(10, 5, LogisticActivation)
+	n.AddHidden(3, true, LogisticActivation)
+	n.Wire()
+	out := n.Predict([]float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1})
+	log.Printf("Output vector: %v\n", out)
+}
+
+func TestWeightLoadSave(t *testing.T) {
+	// Create a new NN
 	n := NewANN(10, 1, LogisticActivation)
-	n.addHidden(5, LogisticActivation)
-	n.wire()
-	for _, inputNode := range n.input {
-		inputNode.val = 1.0
+	n.AddHidden(5, true, LogisticActivation)
+	n.Wire()
+	firstSavedWeights := n.SaveWeights()
+	log.Printf("Num weights saved: %d\n", len(firstSavedWeights))
+	n.LoadWeights(firstSavedWeights)
+	secondSavedWeights := n.SaveWeights()
+	if len(firstSavedWeights) != len(secondSavedWeights) {
+		t.Fatalf("Number of weights changed after saving. %d -> %d", len(firstSavedWeights), len(secondSavedWeights))
 	}
-	n.run()
-	for _, outputNode := range n.output {
-		log.Printf("Output Node: %v\n", outputNode)
+	for i, _ := range firstSavedWeights {
+		if firstSavedWeights[i] != secondSavedWeights[i] {
+			t.Fatalf("Weights at index %d do not match.", i)
+		}
 	}
+}
+
+func TestTrain(t *testing.T) {
+	n := NewANN(1, 1, LogisticActivation)
+	n.AddHidden(3, true, LogisticActivation)
+	n.Wire()
+
+	feats := [][]float64{
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+	}
+	targs := [][]float64{
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+	}
+
 }
