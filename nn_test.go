60aabcbb9e28e642fc7af78db75c3725db28f474
David Findley <david.findley@ou.edu>
2015-09-02 12:59:08 -0500

    Going to implemented reverse pass (dream)

--- nn_test.go
+++ nn_test.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"math"
 	"testing"
+
+	"github.com/tortis/nn/activation"
 )
 
 func TestNNCreate(t *testing.T) {
-	n := NewANN(10, 1, LogisticActivation)
-	n.AddHidden(5, true, LogisticActivation)
+	n := NewANN(10, 1, activation.Logistic)
+	n.AddHidden(5, true, activation.Logistic)
 	if n.NumConnections() != 61 {
 		t.Fatalf("The created network does not have the expected number of connections. %d != %d", 61, n.NumConnections())
 	}
@@ -17,8 +19,8 @@ func TestNNCreate(t *testing.T) {
 }
 
 func TestNNRun(t *testing.T) {
-	n := NewANN(10, 5, LogisticActivation)
-	n.AddHidden(5, true, LogisticActivation)
+	n := NewANN(10, 5, activation.Logistic)
+	n.AddHidden(5, true, activation.Logistic)
 	n.Wire()
 	out := n.Predict([]float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1})
 	log.Printf("Output vector: %v\n", out)
@@ -26,8 +28,8 @@ func TestNNRun(t *testing.T) {
 
 func TestWeightLoadSave(t *testing.T) {
 	// Create a new NN
-	n := NewANN(10, 1, LogisticActivation)
-	n.AddHidden(5, true, LogisticActivation)
+	n := NewANN(10, 1, activation.Logistic)
+	n.AddHidden(5, true, activation.Logistic)
 	n.Wire()
 	firstSavedWeights := n.SaveWeights()
 	log.Printf("Num weights saved: %d\n", len(firstSavedWeights))
@@ -58,8 +60,8 @@ func train(n *ANN) (float64, [][]float64) {
 }
 
 func OffTestTrain(t *testing.T) {
-	n := NewANN(1, 1, LogisticActivation)
-	n.AddHidden(3, true, LogisticActivation)
+	n := NewANN(1, 1, activation.Logistic)
+	n.AddHidden(3, true, activation.Logistic)
 	n.Wire()
 
 	besterr, bestfeats := train(n)
@@ -90,31 +92,40 @@ func OffTestTrain(t *testing.T) {
 
 func TestBP(t *testing.T) {
 	// Create some training data for the sine function on [0,pi]
-	numTP := 8
+	numTP := 10
 	feats := make([][]float64, numTP)
 	targs := make([][]float64, numTP)
 	for i := 0; i < numTP; i++ {
-		x := float64(i) / float64(numTP-1) * math.Pi
-		//x := rand.Float64() * math.Pi
+		x := float64(i)/10*float64(numTP) - 5.0
+		//x := float64(i) / float64(numTP-1) * math.Pi
 		feats[i] = []float64{x}
-		targs[i] = []float64{math.Sin(feats[i][0])}
+		//targs[i] = []float64{math.Sin(feats[i][0])}
+		targs[i] = []float64{activation.Logistic(x)}
 	}
 
 	// Create a nn
-	n := NewANN(1, 1, LogisticActivation)
-	n.AddHidden(25, true, LogisticActivation)
+	n := NewANN(1, 1, activation.Logistic)
+	n.AddHidden(25, true, activation.Logistic)
 	n.Wire()
 
 	// Train the network with the data
-	err := n.Backprop(feats, targs, 200000, 0.02)
+	err := n.Backprop(feats, targs, 200000, 0.02, 1, 0.3)
 	log.Printf("Error after training: %f\n", err)
 
 	log.Println("Validation:")
 	for i := 0; i < len(feats); i++ {
 		fmt.Printf("%f,%f\n", feats[i][0], targs[i][0])
 	}
+	// test sin
+	//for i := 0; i < 50; i++ {
+	//	x := float64(i) / 50 * math.Pi
+	//	r := n.Predict([]float64{x})
+	//	fmt.Printf("%f,,%f\n", x, r[0])
+	//}
+
+	//test logistic
 	for i := 0; i < 50; i++ {
-		x := float64(i) / 50 * math.Pi
+		x := float64(i)/50*10 - 5
 		r := n.Predict([]float64{x})
 		fmt.Printf("%f,,%f\n", x, r[0])
 	}
